3a6059f5134cb0ce2769880db71936616a18f30e
Rémy Ferré <remy-ferre@laposte.net>
2014-08-23 09:56:16 +0200

    Add package template

---
+++ comment-dwim-2.el
@@ -0,0 +1,30 @@
+;;; comment-dwim-2.el --- A comment command to rule them all
+
+;; Copyright (C) 2014  Rémy Ferré
+
+;; Author: Rémy Ferré <remy-ferre@laposte.net>
+;; Keywords: convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+
+
+(provide 'comment-dwim-2)
+;;; comment-dwim-2.el ends here
